e25a0a734b28eecd9d9efe2a0f1b0d0f6d96f123
carogee <cagee@stanford.edu>
2024-10-16 17:22:24 -0700

    initial commit

---
+++ snd_screens.sh
@@ -0,0 +1,6 @@
+#!/bin/bash                                                                                             
+
+source /cds/group/pcds/pyps/conda/dev_conda
+
+pydm /cds/home/c/cagee/SND/snd_corr.ui &
+pydm /cds/home/c/cagee/SND/snd_plots_time.ui &
