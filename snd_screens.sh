a06ccbf538fe4399edb7dc2866f82d4c4d2eafff
carogee <147095356+carogee@users.noreply.github.com>
2024-11-11 17:51:41 -0800

    timetool pv

---
+++ snd_screens.sh
@@ -0,0 +1,7 @@
+#!/bin/bash                                                                                             
+
+source /cds/group/pcds/pyps/conda/dev_conda
+
+pydm /cds/home/c/cagee/SND/snd_corr.ui &
+pydm /cds/home/c/cagee/SND/snd_plots_time.ui &
+python /cds/home/c/cagee/SND/snd_gui.py &
