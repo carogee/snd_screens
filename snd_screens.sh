aaf4c24d8fc3e13bcfa65d12336ca3984b24b09a
carogee <cagee@stanford.edu>
2024-11-15 10:25:22 -0800

    snd screens

--- snd_screens.sh
+++ snd_screens.sh
@@ -3,4 +3,5 @@
 source /cds/group/pcds/pyps/conda/dev_conda
 
 pydm /cds/home/c/cagee/SND/snd_corr.ui &
-pydm /cds/home/c/cagee/SND/snd_plots_time.ui &
+pydm /cds/home/c/cagee/SND/snd_monitoring.ui &
+python /cds/home/c/cagee/SND/snd_gui.py &
